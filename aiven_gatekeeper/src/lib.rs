c3d2e0fd5d70e6b1a36fbf1684ecd24d2851c312
Etienne Stalmans <etienne.stalmans@aiven.io>
2024-01-18 12:43:04 +0100

    some formatting and adding oat hook

--- aiven_gatekeeper/src/lib.rs
+++ aiven_gatekeeper/src/lib.rs
@@ -8,16 +8,29 @@ pgrx::pg_module_magic!();
 
 static mut PREV_PROCESS_UTILITY_HOOK: pg_sys::ProcessUtility_hook_type = None;
 static mut PREV_EXECUTOR_START_HOOK: pg_sys::ExecutorStart_hook_type = None;
+static mut NEXT_OAT_HOOK: pg_sys::object_access_hook_type = None;
 static GUC_IS_STRICT: GucSetting<bool> = GucSetting::<bool>::new(false);
 static GUC_AGENT_IS_ENABLED: GucSetting<bool> = GucSetting::<bool>::new(true);
 
+const OAT_FUNCTION_EXECUTE:u32 = 4; // pgrx doesn't have the enum type for ObjectAccessType
+
+// pgrx doesn't compile the extension correctly if I don't set this macro
+// on atleast one function
 #[pg_extern]
-fn is_enabled() -> bool {
+fn is_true() -> bool {
     return true;
 }
 
 fn is_security_restricted() -> bool {
     return unsafe {pg_sys::InSecurityRestrictedOperation()};
+}
+
+fn is_strict_mode_enabled() -> bool {
+    return GUC_IS_STRICT.get();
+}
+
+fn is_agent_enabled() -> bool {
+    return GUC_AGENT_IS_ENABLED.get();
 }
 
 fn is_elevated() -> bool {
@@ -28,7 +41,7 @@ fn is_elevated() -> bool {
     unsafe {
       let current_user_id: pg_sys::Oid = pg_sys::GetUserId();
       let session_user_id: pg_sys::Oid = pg_sys::GetSessionUserId();
-      
+
       /* short circuit if the current and session user are the same
       * saves on a slightly more expensive role fetch
       */
@@ -51,7 +64,7 @@ fn copy_stmt_checks(stmt: *mut pg_sys::Node) {
     // otherwise, check if we are trying to read from file and are in a context that allows file system access
     if !copy_stmt.filename.is_null() {
         // strict
-        if GUC_IS_STRICT.get() {
+        if is_strict_mode_enabled() {
           pg_sys::error!("COPY TO/FROM FILE not allowed");
         }
         // creating extension
@@ -70,29 +83,29 @@ fn copy_stmt_checks(stmt: *mut pg_sys::Node) {
 }
 
 fn create_extension_checks(stmt: *mut pg_sys::Node) {
-  // get extension statement and name of extension
-  let create_ext_stmt: PgBox<pg_sys::CreateExtensionStmt>;
-  let extname: String;
-  unsafe {
-    create_ext_stmt = PgBox::from_pg(stmt as *mut pg_sys::CreateExtensionStmt);
-      extname= std::ffi::CStr::from_ptr(create_ext_stmt.extname).to_string_lossy().into_owned();
-  }
-  // check if disallowed extension
-  if extname == "file_fdw" { // error and abort the current transaction if disallowed extension
-      pg_sys::error!("{} extension not allowed", extname);
-  }
+    // get extension statement and name of extension
+    let create_ext_stmt: PgBox<pg_sys::CreateExtensionStmt>;
+    let extname: String;
+    unsafe {
+        create_ext_stmt = PgBox::from_pg(stmt as *mut pg_sys::CreateExtensionStmt);
+            extname= std::ffi::CStr::from_ptr(create_ext_stmt.extname).to_string_lossy().into_owned();
+    }
+    // check if disallowed extension
+    if extname == "file_fdw" { // error and abort the current transaction if disallowed extension
+        pg_sys::error!("{} extension not allowed", extname);
+    }
 }
 
 #[pg_guard]
 extern "C" fn executor_start_hook(query_desc: *mut pg_sys::QueryDesc, eflags: i32) {
-  info!("ExecutorStart");
-  unsafe {
-    if let Some(prev_hook) = PREV_EXECUTOR_START_HOOK {
-      prev_hook(query_desc, eflags);
-    } else {
-      pg_sys::standard_ExecutorStart(query_desc, eflags);
+    info!("ExecutorStart");
+    unsafe {
+        if let Some(prev_hook) = PREV_EXECUTOR_START_HOOK {
+            prev_hook(query_desc, eflags);
+        } else {
+            pg_sys::standard_ExecutorStart(query_desc, eflags);
+        }
     }
-  }
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -108,88 +121,120 @@ extern "C" fn process_utility_hook(
   qc: *mut pg_sys::QueryCompletion,
 ) {
 
-  let stmt: *mut pg_sys::Node = unsafe {(*pstmt).utilityStmt };
-  let stmt_type: pg_sys::NodeTag = unsafe { (*stmt).type_ };
+    // only run checks if the agent is actually enabled, just skip all of this
+    if is_agent_enabled() {
+        let stmt: *mut pg_sys::Node = unsafe {(*pstmt).utilityStmt };
+        let stmt_type: pg_sys::NodeTag = unsafe { (*stmt).type_ };
 
-  match stmt_type{
-    pg_sys::NodeTag::T_AlterRoleStmt=>info!("ALTER ROLE STATEMENT"),
-    pg_sys::NodeTag::T_CreateRoleStmt=>info!("CREATE ROLE STATEMENT"),
-    pg_sys::NodeTag::T_DropRoleStmt=>info!("DROP ROLE STATEMENT"),
-    pg_sys::NodeTag::T_GrantRoleStmt=>info!("GRANT ROLE STATEMENT"),
-    pg_sys::NodeTag::T_CopyStmt=>copy_stmt_checks(stmt),
-    pg_sys::NodeTag::T_VariableSetStmt=>(), // currently we don't do any checks on VariableSet
-    pg_sys::NodeTag::T_CreateFunctionStmt=>info!("CREATE FUNCTION STATEMENT"),
-    pg_sys::NodeTag::T_CreateExtensionStmt=>create_extension_checks(stmt),
-    _=> (),
-  }
-
-  unsafe {
-    if let Some(prev_hook) = PREV_PROCESS_UTILITY_HOOK {
-      prev_hook(
-        pstmt,
-        query_string,
-        read_only_tree,
-        context,
-        params,
-        query_env,
-        dest,
-        qc,
-      );
-    } else {
-      pg_sys::standard_ProcessUtility(
-        pstmt,
-        query_string,
-        read_only_tree,
-        context,
-        params,
-        query_env,
-        dest,
-        qc,
-      );
+        match stmt_type{
+            pg_sys::NodeTag::T_AlterRoleStmt=>info!("ALTER ROLE STATEMENT"),
+            pg_sys::NodeTag::T_CreateRoleStmt=>info!("CREATE ROLE STATEMENT"),
+            pg_sys::NodeTag::T_DropRoleStmt=>info!("DROP ROLE STATEMENT"),
+            pg_sys::NodeTag::T_GrantRoleStmt=>info!("GRANT ROLE STATEMENT"),
+            pg_sys::NodeTag::T_CopyStmt=>copy_stmt_checks(stmt),
+            pg_sys::NodeTag::T_VariableSetStmt=>(), // currently we don't do any checks on VariableSet
+            pg_sys::NodeTag::T_CreateFunctionStmt=>info!("CREATE FUNCTION STATEMENT"),
+            pg_sys::NodeTag::T_CreateExtensionStmt=>create_extension_checks(stmt),
+            _=> (),
+        }
     }
-  }
+
+    unsafe {
+        if let Some(prev_hook) = PREV_PROCESS_UTILITY_HOOK {
+            prev_hook(
+            pstmt,
+            query_string,
+            read_only_tree,
+            context,
+            params,
+            query_env,
+            dest,
+            qc,
+            );
+        } else {
+            pg_sys::standard_ProcessUtility(
+            pstmt,
+            query_string,
+            read_only_tree,
+            context,
+            params,
+            query_env,
+            dest,
+            qc,
+            );
+        }
+    }
+}
+
+#[pg_guard]
+extern "C" fn object_access_hook(
+        access: pg_sys::ObjectAccessType,
+        class_id: pg_sys::Oid,
+        object_id: pg_sys::Oid,
+        sub_id: ::std::os::raw::c_int,
+        arg: *mut ::std::os::raw::c_void
+) {
+    // only if the agent is enabled and this prior to the execution of a function
+    if is_agent_enabled() && access == OAT_FUNCTION_EXECUTE {
+        // check object access restrictions
+
+    }
+
+    // continue
+    unsafe {
+        if let Some(prev_hook) = NEXT_OAT_HOOK {
+            prev_hook(access, class_id, object_id, sub_id, arg,);
+        }
+        // there is no else here, unlike ProcessUtility and ExecutorStart, this is the same
+        // as the C version of gatekeeper and is because there is no standard_ObjectAccess function
+        // like there is for standard_ProcessUtility etc
+    }
 }
 
 #[pg_guard]
 pub extern "C" fn _PG_init() {
-  unsafe {
-    // must be loaded as a shared_library
-    if !pg_sys::process_shared_preload_libraries_in_progress {
-      error!("aiven_pg_gatekeeper is not in shared_preload_libraries");
+    unsafe {
+        // must be loaded as a shared_library
+        if !pg_sys::process_shared_preload_libraries_in_progress {
+            error!("aiven_pg_gatekeeper is not in shared_preload_libraries");
+        }
+
+        GucRegistry::define_bool_guc(
+            "aiven.pg_security_agent",
+            "Toggle the security agent checks on and off",
+            "Toggle the security agent checks on and off",
+            &GUC_AGENT_IS_ENABLED,
+            GucContext::Sighup,
+            GucFlags::SUPERUSER_ONLY|GucFlags::DISALLOW_IN_AUTO_FILE|GucFlags::NOT_WHILE_SEC_REST,
+        );
+
+        GucRegistry::define_bool_guc(
+            "aiven.pg_security_agent_strict",
+            "Toggle the agent into strict mode. Reserved actions are blocked regardless of context",
+            "Toggle the agent into strict mode. Reserved actions are blocked regardless of context",
+            &GUC_IS_STRICT,
+            GucContext::Postmaster,
+            GucFlags::SUPERUSER_ONLY|GucFlags::DISALLOW_IN_AUTO_FILE|GucFlags::NOT_WHILE_SEC_REST,
+        );
+
+        PREV_EXECUTOR_START_HOOK = pg_sys::ExecutorStart_hook;
+        pg_sys::ExecutorStart_hook = Some(executor_start_hook);
+
+        PREV_PROCESS_UTILITY_HOOK = pg_sys::ProcessUtility_hook;
+        pg_sys::ProcessUtility_hook = Some(process_utility_hook);
+
+        NEXT_OAT_HOOK = pg_sys::object_access_hook;
+        pg_sys::object_access_hook = Some(object_access_hook)
     }
-    
-    GucRegistry::define_bool_guc(
-      "aiven.pg_security_agent",
-      "Toggle the security agent checks on and off",
-      "Toggle the security agent checks on and off",
-      &GUC_AGENT_IS_ENABLED,
-      GucContext::Sighup,
-      GucFlags::SUPERUSER_ONLY|GucFlags::DISALLOW_IN_AUTO_FILE|GucFlags::NOT_WHILE_SEC_REST,
-  );
-
-    GucRegistry::define_bool_guc(
-        "aiven.pg_security_agent_strict",
-        "Toggle the agent into strict mode. Reserved actions are blocked regardless of context",
-        "Toggle the agent into strict mode. Reserved actions are blocked regardless of context",
-        &GUC_IS_STRICT,
-        GucContext::Postmaster,
-        GucFlags::SUPERUSER_ONLY|GucFlags::DISALLOW_IN_AUTO_FILE|GucFlags::NOT_WHILE_SEC_REST,
-    );
-
-    PREV_EXECUTOR_START_HOOK = pg_sys::ExecutorStart_hook;
-    pg_sys::ExecutorStart_hook = Some(executor_start_hook);
-
-    PREV_PROCESS_UTILITY_HOOK = pg_sys::ProcessUtility_hook;
-    pg_sys::ProcessUtility_hook = Some(process_utility_hook);
-  }
 }
 
 #[pg_guard]
 pub extern "C" fn _PG_fini() {
-  unsafe {
-    pg_sys::ExecutorStart_hook = PREV_EXECUTOR_START_HOOK;
-    pg_sys::ProcessUtility_hook = PREV_PROCESS_UTILITY_HOOK;
-  }
+    unsafe {
+        pg_sys::ExecutorStart_hook = PREV_EXECUTOR_START_HOOK;
+        pg_sys::ProcessUtility_hook = PREV_PROCESS_UTILITY_HOOK;
+        pg_sys::object_access_hook = NEXT_OAT_HOOK;
+    }
 }
 
 #[cfg(any(test, feature = "pg_test"))]
